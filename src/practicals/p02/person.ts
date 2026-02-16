65f2508833fffb61ba6e3244d590f586e621d71f
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-02-16 09:56:31 +0700

    TT

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,26 @@
-export class Person {}
+export class Person {
+    static COUNTRY = 'Thailand'; // หรือค่าคงที่ที่กำหนดเอง
+    firstname?: string;
+    lastname?: string;
+    age?: number;
+    
+    constructor(firstname?: string, lastname?: string, age?: number) {
+        this.firstname = firstname || 'John';
+        this.lastname = lastname || 'Doe';
+        this.age = age;
+    }
+    printName(): string {
+        const FullName = `${this.firstname} ${this.lastname}`;
+        console.log(FullName);
+        return FullName;
+    }
+    getFullName(): string {
+        return `${this.firstname} ${this.lastname}`;
+    }
+    setAge(age: number): void {
+        this.age = age;
+    }
+    getAge(): number | undefined {
+        return this.age;
+    }
+}
