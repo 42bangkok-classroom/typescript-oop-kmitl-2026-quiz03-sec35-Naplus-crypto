1f9da5f0cfa77426dfb25823c500a48f7e554648
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-02-16 10:12:17 +0700

    try

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -4,15 +4,6 @@ export class Person {
     lastname?: string;
     age?: number = 25;
     
-    constructor(firstname?: string, lastname?: string, age?: number) {
-        this.firstname = firstname || 'John';
-        this.lastname = lastname || 'Doe';
-        this.age = age || 25;
-    }
-    printName(): string {
-        const FullName = `${this.firstname} ${this.lastname}`;
-        return FullName;
-    }
     getFullName() {
         return `${this.firstname} ${this.lastname}`;
     }
