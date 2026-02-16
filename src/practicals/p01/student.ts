a7bdec0e8a654b02b0aa8f9a95d3abb7664adde7
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-02-16 09:27:06 +0700

    firstpush

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,23 @@
-export class Student {}
+export class Student {
+  firstname: string;
+  lastname: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstname = firstName;
+    this.lastname = lastName;
+  }
+
+//    getFirstName(): string {
+//      return this.firstname;
+//  }
+//
+//    getLastName(): string {
+//        return this.lastname;
+//  }
+
+  printName(): string {
+    const fullName = `${this.firstname} ${this.lastname}`;
+    console.log(fullName);
+    return fullName;
+  }
+}
