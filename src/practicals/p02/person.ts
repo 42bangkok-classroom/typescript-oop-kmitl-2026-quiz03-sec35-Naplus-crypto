3954c5e24040a2bf2c2fca3f675f39b9cbb98932
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-02-16 10:08:11 +0700

    เอา comsole.log ออกละ

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -2,22 +2,21 @@ export class Person {
     static COUNTRY = 'Thailand'; // หรือค่าคงที่ที่กำหนดเอง
     firstname?: string;
     lastname?: string;
-    age?: number;
+    age?: number = 25;
     
     constructor(firstname?: string, lastname?: string, age?: number) {
         this.firstname = firstname || 'John';
         this.lastname = lastname || 'Doe';
-        this.age = age;
+        this.age = age || 25;
     }
     printName(): string {
         const FullName = `${this.firstname} ${this.lastname}`;
-        console.log(FullName);
         return FullName;
     }
-    getFullName(): string {
+    getFullName() {
         return `${this.firstname} ${this.lastname}`;
     }
-    setAge(age: number): void {
+    setAge(age: number) {
         if(isNaN(age)) {
             this.age = age;
         }
