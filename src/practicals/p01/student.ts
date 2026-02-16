60b21e4bd10558db708c5cb16a5d2d67de267415
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-02-16 09:45:38 +0700

    เข้าใจละ

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,10 +1,10 @@
 export class Student {
-  firstname: string;
-  lastname: string;
+  firstname?: string;
+  lastname?: string;
 
-  constructor(firstname: string, lastname: string) {
-    this.firstname = firstname = 'John';
-    this.lastname = lastname = 'Doe';
+  constructor(firstname?: string, lastname?: string) {
+    this.firstname = firstname || 'John';
+    this.lastname = lastname || 'Doe';
   }
   printName(): string {
     const fullName = `${this.firstname} ${this.lastname}`;
